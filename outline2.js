e191cfbb4ff8fe95ab691a4811fc9ce8ffd44360
augustx28 <wally.esaa@gmail.com>
2026-08-12 21:13:20 -0400

    Change chat opacity from 0.1 to 0.5

--- outline2.js
+++ outline2.js
@@ -168,7 +168,7 @@
       }
 
       #${TOGGLE_ID}.chat-visible {
-        opacity: 0.1;
+        opacity: 0.5;
         visibility: visible;
         pointer-events: auto;
         transform: translateY(0) scale(1);
